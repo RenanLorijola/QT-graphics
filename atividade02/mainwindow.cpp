ed90f781ef49d0b056a61527b31490beed5fcc16
Renan Zago Lorijola <renanzago@gmail.com>
2022-09-29 09:52:30 -0300

    centring triangle, circle and square

--- atividade02/mainwindow.cpp
+++ atividade02/mainwindow.cpp
@@ -18,3 +18,40 @@ void MainWindow::SelectedDraw(int d){
     typeOfDraw=d;
     update();
 }
+
+int centerX(int screenWidth, int width){
+    return (screenWidth-width)/2;
+}
+
+void MainWindow::paintEvent(QPaintEvent *){
+    QPainter painter(this);
+
+    int screenWidth = 250;
+    int width = 30;
+    switch(typeOfDraw){
+        case 0:
+            break;
+        case 1:
+            width = 30;
+            painter.drawRect(centerX(screenWidth,width),30,width,60);
+            break;
+        case 2:
+            width = 40;
+            painter.drawEllipse(centerX(screenWidth,width),30,width,width);
+            break;
+        case 3:
+            width = 45;
+            painter.drawRect(centerX(screenWidth,width),30,width,width);
+            break;
+        case 4:
+            width = 45;
+            painter.drawRect(centerX(screenWidth,width),30,width,width);
+            break;
+        case 5:
+            width = 45;
+            painter.rotate(45);
+            painter.drawRect(centerX(screenWidth,width),30,width,width);
+            break;
+    }
+
+}
